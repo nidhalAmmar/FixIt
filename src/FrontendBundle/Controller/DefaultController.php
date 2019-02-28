366d23b06d0b36c0d43cfd270ebc5082e646a5e8
nidhal ammar <nidhal.ammar12@gmail.com>
2019-02-28 03:50:16 +0100

    final commit

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -4,11 +4,15 @@ namespace FrontendBundle\Controller;
 
 use AdminUserBundle\Entity\User;
 use ClientBundle\Entity\Demande;
+use FrontendBundle\Entity\Reclamation;
+use FrontendBundle\Form\ReclamationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use ServiceBundle\Entity\Service;
 use OffreBundle\Form\OffreType;
 use OffreBundle\Entity\Offre;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends Controller
 {
@@ -48,7 +52,7 @@ class DefaultController extends Controller
 
     }
 
-    public function servicesAction()
+    public function OffresAction()
     {
         $user = $this->getUser();
         $user->getId();
@@ -59,15 +63,43 @@ class DefaultController extends Controller
         $services = $this->getDoctrine()->getRepository(Service::class)->findAll();
         return $this->render('@Frontend/Default/MesOffres.html.twig', array('services'=>$services ,'offres' =>$offres));
     }
+    public function servicesAction()
+    {
+        $user = $this->getUser();
+        $user->getId();
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->getRepository('ServiceBundle:Service')->createQueryBuilder('d')->where('d.User =:id')
+            ->setParameter('id',$user)->getQuery();
+        $services = $qb->getResult();
+        return $this->render('@Frontend/Default/MesServices.html.twig', array('services'=>$services ));
+    }
 
     public function aboutusAction()
     {
         return $this->render('@Frontend/Default/aboutus.html.twig');
     }
 
-    public function contactusAction()
+    public function contactusAction(Request $request)
     {
-        return $this->render('@Frontend/Default/contactus.html.twig');
+        //return $this->render('@Frontend/Default/contactus.html.twig');
+        $reclamation = new Reclamation();
+        $form = $this->createForm(ReclamationType::class, $reclamation);
+        $form = $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+            $reclamation->setDate( new \DateTime('now'));
+            $reclamation->setUser($this->getUser());
+            $reclamation->setEtat("unread");
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($reclamation);
+            $em->flush();
+
+            return $this->redirectToRoute("fixit_homepage");
+
+        }
+        return $this->render('@Frontend/Default/contactus.html.twig', array(
+            'form'   => $form->createView(),
+        ));
     }
 
     public function galleryAction()
@@ -95,6 +127,28 @@ class DefaultController extends Controller
             'form'   => $form->createView(),
         ));
        // return $this->render('@Frontend/Default/AddOffre.html.twig');
+    }
+    public function profilAction()
+    {
+        return $this->render('@Frontend/Default/profil.html.twig');
+    }
+    public function profilImprimerAction()
+    {
+        $snappy = $this->get("knp_snappy.pdf");
+        $html = $this->renderView('@Frontend/Default/profil.html.twig');
+        $filename= "custom_pdf_from_twig";
+
+        return new Response(
+            $snappy->getOutputFromHtml($html,array(
+                'lowquality' => false,
+                'encoding' => 'utf-8',
+            )),
+            200,
+            array(
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'inline; filename="'.$filename.'.pdf"'
+            )
+        );
     }
 
     public function AddServiceAction()
